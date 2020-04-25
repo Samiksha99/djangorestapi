ed3e89b7755cfbe38b5e88dd42d2e03aa60a6ac1
Samiksha Agrawal <agrawalsamiksha25@gmail.com>
2020-04-26 01:06:40 +0530

    table available

--- intern/settings.py
+++ intern/settings.py
@@ -77,9 +77,11 @@ WSGI_APPLICATION = 'intern.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME':'account',
+        'USER': 'postgres',
+        'PASSWORD': 'quad_sas123',
+        'HOST':'localhost'    }
 }
 
 
